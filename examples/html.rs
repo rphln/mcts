f98449aef2aa433a907c4fcce6060cac9aca87cf
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2025-09-29 08:21:54 -0300

    game: revert to square grid
    While I figure out why it is so much faster.

--- examples/html.rs
+++ examples/html.rs
@@ -183,8 +183,8 @@ fn label(mov: &Move) -> String {
             format!(
                 "🧭 {label} → ⟨{x}, {y}⟩",
                 label = CHARACTER[character.0],
-                x = destination.q,
-                y = destination.r,
+                x = destination.x,
+                y = destination.y,
             )
         }
         Move::Action { action, target_hint: Some(target), .. } => {
@@ -198,8 +198,8 @@ fn label(mov: &Move) -> String {
             format!(
                 "🎯 {label} → ⟨{x}, {y}⟩",
                 label = ACTION[action.0],
-                x = destination.q,
-                y = destination.r,
+                x = destination.x,
+                y = destination.y,
             )
         }
     }
