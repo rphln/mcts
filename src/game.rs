4236f9fde79dcc2dc93eb986b249187454ed8f9e
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2025-10-04 13:41:02 -0300

    mcts: account for block and poison in eval function

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use std::cmp;
+use std::cmp::{max, min};
 
 use swift_swallow::{
     command::Command,
@@ -52,25 +52,41 @@ impl Game {
 
     #[must_use]
     pub fn evaluate(&self) -> f64 {
-        let mut max = 0.;
-        let mut min = 0.;
+        /// How much health a point of energy is worth, on average.
+        const K: f64 = 8.;
+
+        let mut max_score = 0.;
+        let mut min_score = 0.;
 
         for character in &self.world.characters {
-            let health = f64::from(character.current_effective_health());
-            let energy = f64::from(cmp::min(
+            if character.is_defeated() {
+                continue;
+            }
+
+            let turn_health = f64::from({
+                let health = character.current_health();
+
+                let block = character.block;
+                let poison = max(character.markers.poison_until - self.world.round, 0);
+
+                let turn_health = min(health + block, health - poison);
+                max(turn_health, 0)
+            });
+
+            let turn_energy = f64::from(min(
                 character.maximum_action_points,
                 character.current_action_points + 4,
             ));
 
-            let points = (64. * health + 8. * energy).sqrt();
+            let points = (turn_health + K * turn_energy).sqrt();
 
             if character.team == self.color {
-                max += points;
+                max_score += points;
             } else {
-                min += points;
+                min_score += points;
             }
         }
 
-        max - min
+        max_score - min_score
     }
 }
