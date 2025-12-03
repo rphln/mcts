0ae2e784ee8ca8498e8198991457fab8b3bd3ad2
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2025-12-03 08:01:09 -0300

    mcts: reduce `unsafe` scope in `thread_time`

--- src/mcts.rs
+++ src/mcts.rs
@@ -425,13 +425,9 @@ impl Mcts {
 fn thread_time() -> Duration {
     let mut time = libc::timespec { tv_sec: 0, tv_nsec: 0 };
 
-    unsafe {
-        assert_ne!(
-            libc::clock_gettime(libc::CLOCK_THREAD_CPUTIME_ID, &raw mut time),
-            -1,
-            "Failed to get CPU time"
-        );
-    };
+    let err =
+        unsafe { libc::clock_gettime(libc::CLOCK_THREAD_CPUTIME_ID, &raw mut time) };
+    assert_ne!(err, -1, "Failed to get CPU time");
 
     let secs = u64::try_from(time.tv_sec).unwrap();
     let nanos = u32::try_from(time.tv_nsec).unwrap();
